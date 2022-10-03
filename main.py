4f21fcdd098b5e3e46416da09cfa8db60496a976
大镖客 <2249413352@qq.com>
2022-10-03 15:40:22 +0800

    15:40

--- main.py
+++ main.py
@@ -5,7 +5,30 @@
 
 
 def print_hi(name):
+    data=[]
+    with open('dataset_tsmc2014/dataset_TSMC2014_TKY.txt','r',encoding='utf8')as f:
+
+        lines=f.readlines()
+        for line in lines:
+            print(line)
+            line=line[:-1].split('\t')
+            User_ID=int(line[0])
+            Venue_ID=line[1]
+            Venue_category_ID=line[2]
+            Venue_category_name=line[3]
+            Latitude=line[4]
+            Longitude=line[5]
+            Timezone_offset_in_minutes=line[6]
+            UTC_time=line[7]
     # 在下面的代码行中使用断点来调试脚本。
+    # 1. User ID (anonymized)
+    # 2. Venue ID (Foursquare)
+    # 3. Venue category ID (Foursquare)
+    # 4. Venue category name (Fousquare)
+    # 5. Latitude
+    # 6. Longitude
+    # 7. Timezone offset in minutes (The offset in minutes between when this check-in occurred and the same time in UTC)
+    # 8. UTC time
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
 
